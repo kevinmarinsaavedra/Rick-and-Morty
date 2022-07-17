e85f3f649e0e924b3659d0c56c346f38e378183e
Kevin Marin <kevinmarinsaavedra@gmail.com>
2022-07-17 12:31:28 -0400

    Add app icons

---
+++ Rick and Morty/Rick and Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty
+//
+//  Created by Kevin Marin on 17/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
